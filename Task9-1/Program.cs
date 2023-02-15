be704953c900f000ab8c66d183a9842690b12612
Axiometer <120444103+Axiometer@users.noreply.github.com>
2023-02-15 09:23:11 +0300

    v1

---
+++ Task9-1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии
+
+// Ввод числа пользователем
+int GetNumberFromUser(string message)
+{
+    while(true)     
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)       
+            return userNumber;
+        Console.WriteLine("Ошибка ввода!");
+    }
+}
+
+// Рекурсионный вывод
+void WriteNumbersFromMToN(int m, int n)
+{
+    if (m % 2 != 0)
+        m ++;
+
+    if (n < m && m % 2 == 0)
+        return;
+
+    Console.Write(m + " ");
+    WriteNumbersFromMToN(m + 2, n);
+}
+
+System.Console.WriteLine("Вывод всех чётных натуральных чисел в промежутке от M до N с помощью рекурсии.");
+int M = GetNumberFromUser("Введите нижний порог M: ");
+int N = GetNumberFromUser("Введите верхний порог N: ");
+
+WriteNumbersFromMToN(M, N);
---
+++ Task9-2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+
+// Ввод числа пользователем
+int GetNumberFromUser(string message)
+{
+    while(true)     
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)       
+            return userNumber;
+        Console.WriteLine("Ошибка ввода!");
+    }
+}
+
+// Рекурсионный подсчет суммы
+int SumMN(int m, int n)
+{
+    if (m > n) 
+        return 0;
+    return n + SumMN(m, n - 1);
+}
+
+System.Console.WriteLine("Сумма натуральных элементов в промежутке от M до N");
+int M = GetNumberFromUser("Введите первое число  M: ");
+int N = GetNumberFromUser("Введите последнее число N: ");
+
+System.Console.Write($"Сумма натуральных чисел от {M} до {N} равна {SumMN(M, N)}");
+
